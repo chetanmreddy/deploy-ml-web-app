e330c099c3c91cedd0fc3b85da962e395549fcd3
Chetan Reddy <chetanmreddy@outlook.com>
2021-01-05 18:57:53 -0800

    refactored the train code for OOP's

--- train.py
+++ train.py
@@ -7,61 +7,7 @@ import torch.optim as optim
 import logging
 import numpy as np
 
-# Define transformers
-
-transform_train = transforms.Compose(
-    [
-        transforms.RandomCrop(28, padding=4),
-        transforms.ToTensor(),
-        # transforms.Normalize((mean,), (std,)),
-    ]
-)
-
-transform_valid = transforms.Compose(
-    [
-        transforms.ToTensor(),
-        # transforms.Normalize((mean,), (std,)),
-    ]
-)
-
-
-# Fixed Params
-root = ""
-
-batch_size = 128
-test_batch_size = 128
-lr = 1e-3
-EPOCHS = 200
-
-
-# Prepare loaders
-
-train = datasets.EMNIST(
-    root, split="balanced", train=True, download=True, transform=transform_train
-)
-test = datasets.EMNIST(
-    root, split="balanced", train=False, download=True, transform=transform_valid
-)
-
-
-train_loader = torch.utils.data.DataLoader(
-    train, batch_size=batch_size, shuffle=True, num_workers=2, drop_last=True
-)
-
-
-test_loader = torch.utils.data.DataLoader(
-    test, batch_size=test_batch_size, shuffle=False, num_workers=2, drop_last=True
-)
-
-# Define model
-
-
-def init_weights(m):
-    if type(m) == nn.Linear:
-        torch.nn.init.xavier_uniform_(m.weight)
-        m.bias.data.fill_(0.01)
-
-
+# Define the network used for training
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -109,68 +55,63 @@ class Net(nn.Module):
         return x
 
 
-net = Net()
-net.apply(init_weights)
 
+# Define transformers to applpy on the input data
+
+transform_train = transforms.Compose(
+    [
+        transforms.RandomCrop(28, padding=4),
+        transforms.ToTensor(),
+        # transforms.Normalize((mean,), (std,)),
+    ]
+)
+
+transform_valid = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        # transforms.Normalize((mean,), (std,)),
+    ]
+)
+
+def init_weights(m):
+    if type(m) == nn.Linear:
+        torch.nn.init.xavier_uniform_(m.weight)
+        m.bias.data.fill_(0.01)
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
+def train():
+    # Fixed Params
+    root = ""
 
-print(count_parameters(net))
+    batch_size = 128
+    test_batch_size = 128
+    lr = 1e-3
+    EPOCHS = 200
 
+    net = Net()
+    net.apply(init_weights)
 
-logging.basicConfig(filename="cnn2.log", level=logging.DEBUG)
-logging.info(net)
-logging.info("Number of parameters: {}".format(count_parameters(net)))
+    print(count_parameters(net))
 
+    logging.basicConfig(filename="cnn2.log", level=logging.DEBUG)
+    logging.info(net)
+    logging.info("Number of parameters: {}".format(count_parameters(net)))
 
-# Training
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # Training
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+    data_gen = inf_generator(train_loader)
+    batches_per_epoch = len(train_loader)
 
-def one_hot(x, K):
-    return np.array(x[:, None] == np.arange(K)[None, :], dtype=int)
+    optimizer = torch.optim.SGD(net.parameters(), lr=lr, momentum=0.9)
+    criterion = nn.CrossEntropyLoss().to(device)
 
+    best_acc = 0
+    net.to(device)
 
-def inf_generator(iterable):
-    """Allows training with DataLoaders in a single infinite loop:
-        for i, (x, y) in enumerate(inf_generator(train_loader)):
-    """
-    iterator = iterable.__iter__()
-    while True:
-        try:
-            yield iterator.__next__()
-        except StopIteration:
-            iterator = iterable.__iter__()
-
-
-def accuracy(model, dataset_loader):
-    total_correct = 0
-    for x, y in dataset_loader:
-        x = x.view(-1, 28, 28, 1)
-        x = torch.transpose(x, 1, 2)
-
-        x = x.to(device)
-        y = one_hot(np.array(y.numpy()), 47)
-
-        target_class = np.argmax(y, axis=1)
-        predicted_class = np.argmax(model(x).cpu().detach().numpy(), axis=1)
-        total_correct += np.sum(predicted_class == target_class)
-    return total_correct / len(dataset_loader.dataset)
-
-
-data_gen = inf_generator(train_loader)
-batches_per_epoch = len(train_loader)
-
-
-optimizer = torch.optim.SGD(net.parameters(), lr=lr, momentum=0.9)
-criterion = nn.CrossEntropyLoss().to(device)
-
-best_acc = 0
-net.to(device)
-
-for itr in range(EPOCHS * batches_per_epoch):
+    for itr in range(EPOCHS * batches_per_epoch):
 
     optimizer.zero_grad()
     x, y = data_gen.__next__()
@@ -205,3 +146,56 @@ for itr in range(EPOCHS * batches_per_epoch):
                     itr // batches_per_epoch, train_acc, val_acc
                 )
             )
+
+def loader():
+    train = datasets.EMNIST(
+        root, split="balanced", train=True, download=True, transform=transform_train
+    )
+    test = datasets.EMNIST(
+        root, split="balanced", train=False, download=True, transform=transform_valid
+    )
+
+
+    train_loader = torch.utils.data.DataLoader(
+        train, batch_size=batch_size, shuffle=True, num_workers=2, drop_last=True
+    )
+
+
+    test_loader = torch.utils.data.DataLoader(
+        test, batch_size=test_batch_size, shuffle=False, num_workers=2, drop_last=True
+    )  
+
+if __name__ == "__main__":
+    loader()
+    train()
+
+
+def one_hot(x, K):
+    return np.array(x[:, None] == np.arange(K)[None, :], dtype=int)
+
+
+def inf_generator(iterable):
+    """Allows training with DataLoaders in a single infinite loop:
+        for i, (x, y) in enumerate(inf_generator(train_loader)):
+    """
+    iterator = iterable.__iter__()
+    while True:
+        try:
+            yield iterator.__next__()
+        except StopIteration:
+            iterator = iterable.__iter__()
+
+
+def accuracy(model, dataset_loader):
+    total_correct = 0
+    for x, y in dataset_loader:
+        x = x.view(-1, 28, 28, 1)
+        x = torch.transpose(x, 1, 2)
+
+        x = x.to(device)
+        y = one_hot(np.array(y.numpy()), 47)
+
+        target_class = np.argmax(y, axis=1)
+        predicted_class = np.argmax(model(x).cpu().detach().numpy(), axis=1)
+        total_correct += np.sum(predicted_class == target_class)
+    return total_correct / len(dataset_loader.dataset)
